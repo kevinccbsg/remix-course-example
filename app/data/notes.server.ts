fffe3c7d1fb52d12e4f0c2991e88c257fbf17b93
kevinccbsg <kevinccbsg@gmail.com>
2023-12-10 21:54:28 +0100

    feat: add server

--- app/data/notes.server.ts
+++ app/data/notes.server.ts
@@ -1,18 +1,16 @@
-import fs from 'fs/promises';
-
 interface Note {
   id: string;
   title: string;
   content: string;
 }
 
+let notes: Note[] = [];
+
 export async function getStoredNotes() {
-  const rawFileContent = await fs.readFile('notes.json', { encoding: 'utf-8' });
-  const data = JSON.parse(rawFileContent);
-  const storedNotes = data.notes ?? [];
-  return storedNotes as Note[];
+  return notes as Note[];
 }
 
-export function storeNotes(notes: Note[]) {
-  return fs.writeFile('notes.json', JSON.stringify({ notes: notes || [] }));
+export function storeNotes(newNotes: Note[]) {
+  notes = [...notes, ...newNotes];
+  return;
 }
